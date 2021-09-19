bcf3f664583d21959b88407a6a66f83bff65eac4
Eldio Santos <eldio.santos@m4u.com.br>
2021-09-19 15:35:50 -0300

    Added a README file

---
+++ README.md
@@ -0,0 +1,7 @@
+# webcomics-fetcher2-oots #
+
+It's just a POC to test the use of plugins in a Go project (it's one plugin)...
+
+## main project ##
+
+- [webcomics-fetcher2-go](https://github.com/Eldius/webcomics-fetcher2-go)
--- helper/helper.go
+++ helper/helper.go
@@ -1,15 +1,13 @@
 package helper
 
 import (
-	"plugin"
-
 	"github.com/Eldius/webcomics-fetcher2-go/plugins"
 )
 
 func PluginInfo() *plugins.Plugin {
 	return &plugins.Plugin{
 		Name:        "oots",
-		Path:        plugin.GetBinaryPath(),
+		Path:        plugins.GetBinaryPath(),
 		Description: "Order of The Stick plugin",
 	}
 }
